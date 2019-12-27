271221fb3841b909dbbc4c035ca74692dab1d915
suresh <suresh.gadekar01@gmail.com>
2019-12-28 00:02:31 +0530

    commit 4

---
+++ demogit4/src/demogit4/DemoEntity.java
@@ -0,0 +1,21 @@
+package demogit4;
+
+public class DemoEntity {
+
+	private String name;
+	private String password;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
